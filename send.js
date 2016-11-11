08010f6970a063e493e91f47be5755c83eb4e84c
yurakravtsiv <yura.kravtsiv@gmail.com>
2016-11-11 11:50:05 +0200

    test

---
+++ send.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+        debugger;
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+            type: "GET", //Метод отправки
+            url: "send.php", //путь до php фаила отправителя
+            data: form_data,
+            success: function() {
+                   //код в этом блоке выполняется при успешной отправке сообщения
+                   alert("Ваше сообщение отпрвлено!");
+            },
+                error: function(jqXHR, textStatus, errorThrown) {
+                   //код в этом блоке выполняется при успешной отправке сообщения
+                   alert(textStatus);
+                   alert(errorThrown);
+            }
+        });
+        debugger;
+    });  
+});

d83e2e7303c839348076062e0c080c5b2798d5da
yurakravtsiv <yura.kravtsiv@gmail.com>
2016-11-11 12:06:23 +0200

    test6

--- send.php
+++ send.php
@@ -1,19 +1,10 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "yura.kravtsiv@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("yura.kravtsiv@gmail.com","My subject",$msg);
 ?>
